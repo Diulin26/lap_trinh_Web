bfb82c7ddfb132adc33e63a72957efabb4a970b8
Diulin26 <ledieulinh3011@gmail.com>
2026-08-15 10:53:17 +0700

    Complete Student class properties and constructor

--- LTDL_lesson01/LTDL_lesson01/Student.cs
+++ LTDL_lesson01/LTDL_lesson01/Student.cs
@@ -6,14 +6,42 @@ using System.Threading.Tasks;
 
 namespace LTDLLesson01
 {
-    
+    /// <summary>
     /// Class Student
     /// Author: LeLinh
-  
+    /// </summary>
     internal class Student
     {
         public string mssv { get; set; }
         public string name { get; set; }
         public DateTime? Ngaysinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public string NganhHoc { get; set; }
+        public double DiemTrungBinh { get; set; }
+        public string TrangThaiHocTap { get; set; }
+
+        public Student(
+            string mssv,
+            string name,
+            DateTime? ngaysinh,
+            string gioiTinh,
+            string email,
+            string soDienThoai,
+            string nganhHoc,
+            double diemTrungBinh,
+            string trangThaiHocTap)
+        {
+            this.mssv = mssv;
+            this.name = name;
+            this.Ngaysinh = ngaysinh;
+            this.GioiTinh = gioiTinh;
+            this.Email = email;
+            this.SoDienThoai = soDienThoai;
+            this.NganhHoc = nganhHoc;
+            this.DiemTrungBinh = diemTrungBinh;
+            this.TrangThaiHocTap = trangThaiHocTap;
+        }
     }
 }
