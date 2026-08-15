9bf4fbb9f172dd7e0b0ddece928e24c975bcfd47
Diulin26 <ledieulinh3011@gmail.com>
2026-08-15 10:06:24 +0700

    Create Student class and initial project

---
+++ LTDL_lesson01/LTDL_lesson01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LTDLLesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string choise;
+            List<Student> students = new List<Student>()
+            {
+                new Student
+             {
+                    mssv = "001",
+                    name = "John Doe"
+                },
+
+                new Student
+                {
+                    mssv = "002",
+                    name = "Jane Smith"
+                }
+            };
+            do
+            {
+                menu();
+                Console.WriteLine("Enter your choice: ");
+                choise = Console.ReadLine();
+                switch (choise)
+                {
+                    case "1":
+                        // Them sv
+                        break;
+                    case "2":
+                        //Hien thi sv
+                        break;
+                    case "14":
+                        Console.WriteLine("Ban da ket thuc chuc nang");
+                        break;
+                    default: Console.WriteLine("Sai chuc nang, vui long chon lai");
+                        break;
+                }
+            } while (choise != "0");
+        }
+   
+        static void menu()
+        {
+            Console.WriteLine("============CHUC NANG============");
+            Console.WriteLine("1. Them sinh vien"); 
+
+
+        }
+    }
+
+}
---
+++ LTDL_lesson01/LTDL_lesson01/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDLLesson01
+{
+    
+    /// Class Student
+    /// Author: LeLinh
+  
+    internal class Student
+    {
+        public string mssv { get; set; }
+        public string name { get; set; }
+        public DateTime? Ngaysinh { get; set; }
+    }
+}
