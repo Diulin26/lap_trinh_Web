6adaf2ae8288ad4001bdd6ba40cb21394fe8d9c0
Diulin26 <ledieulinh3011@gmail.com>
2026-08-23 18:47:20 +0700

    Upload LTDL_Lesson_02

---
+++ MyAppMVC/MyAppMVC/Controllers/DemoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppMVC.Controllers
+{
+    public class DemoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
