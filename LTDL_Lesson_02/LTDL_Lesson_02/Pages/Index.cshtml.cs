5037864ccdfd27e1d3e9cdb1d39e4d314b06b5bd
Diulin26 <ledieulinh3011@gmail.com>
2026-08-23 18:56:44 +0700

    Restore LTDL_Lesson_02 complete

---
+++ LTDL_Lesson_02/LTDL_Lesson_02/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LTDL_Lesson_02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
